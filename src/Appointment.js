c7d84a7ef342a6863569ab305eb082c8b2e4097b
kimkozlov <kimkozlov.ks@gmail.com>
2021-12-17 13:28:41 +0200

    Solved Chapter 1 exercise

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
-export const Appointment = ({ customer: { firstName } }) => ( <div>{firstName}</div>
+export const Appointment = ({ customer, stylist, service, notes, startAt}) => (
+  <>
+    <h4>{`Today's appointment at ${startAt}`}</h4>
+    <table>
+      <tbody>
+        <tr>
+          <td>Customer</td>
+          <td>{customer.firstName} {customer.lastName}</td>
+        </tr>
+        <tr>
+          <td>Phone number</td>
+          <td>{customer.phoneNumber}</td>
+        </tr>
+        <tr>
+          <td>Stylist</td>
+          <td>{stylist}</td>
+        </tr>
+        <tr>
+          <td>Service</td>
+          <td>{service}</td>
+        </tr>
+        <tr>
+          <td>Notes</td>
+          <td>{notes}</td>
+        </tr>
+      </tbody>
+    </table>
+  </>
 );
 
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -2,6 +2,7 @@ import {describe, it, beforeEach} from "@jest/globals";
 import { Appointment} from '../src/Appointment';
 import React from "react";
 import ReactDOM from 'react-dom';
+import {sampleAppointments} from "../src/sampleData";
 
 describe('Appointment', () => {
   let container;
@@ -23,6 +24,105 @@ describe('Appointment', () => {
     render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch('Jourdan');
   });
+
+  it('renders the two columns table', () => {
+    render(<Appointment customer={sampleAppointments[0].customer} />);
+    expect(container.querySelector('table')).not.toBeNull();
+    expect(
+      container.querySelector('table > tbody > tr').children
+    ).toHaveLength(2);
+  });
+
+  it('renders customer first and last name in the first row', () => {
+    render(<Appointment customer={sampleAppointments[0].customer} />);
+    expect(
+      container.querySelector('table > tbody > tr')
+        .children[0]
+        .textContent).toMatch('Customer')
+    expect(
+      container.querySelector('table > tbody > tr')
+        .children[1]
+        .textContent).toMatch(
+          `${sampleAppointments[0].customer.firstName} ${sampleAppointments[0].customer.lastName}`)
+  });
+
+  it('renders customer phone number in the second row', () => {
+    render(<Appointment customer={sampleAppointments[0].customer} />);
+    expect(
+      container.querySelector('table > tbody')
+        .children[1].children[0]
+        .textContent).toMatch('Phone number')
+    expect(
+      container.querySelector('table > tbody')
+        .children[1].children[1]
+        .textContent).toMatch(
+      `${sampleAppointments[0].customer.phoneNumber}`)
+  });
+
+  it('renders stylist in the third row', () => {
+    render(<Appointment
+      customer={sampleAppointments[0].customer}
+      stylist={sampleAppointments[0].stylist} />);
+    expect(
+      container.querySelector('table > tbody')
+        .children[2].children[0]
+        .textContent).toMatch('Stylist')
+    expect(
+      container.querySelector('table > tbody')
+        .children[2].children[1]
+        .textContent).toMatch(
+      `${sampleAppointments[0].stylist}`)
+  });
+
+  it('renders service in the 4th row', () => {
+    render(<Appointment
+      customer={sampleAppointments[0].customer}
+      stylist={sampleAppointments[0].stylist}
+      service={sampleAppointments[0].service}
+    />);
+    expect(
+      container.querySelector('table > tbody')
+        .children[3].children[0]
+        .textContent).toMatch('Service')
+    expect(
+      container.querySelector('table > tbody')
+        .children[3].children[1]
+        .textContent).toMatch(
+      `${sampleAppointments[0].service}`)
+  });
+
+  it('renders notes in the 5th row', () => {
+    render(<Appointment
+      customer={sampleAppointments[0].customer}
+      stylist={sampleAppointments[0].stylist}
+      service={sampleAppointments[0].service}
+      notes={sampleAppointments[0].notes}
+    />);
+    expect(
+      container.querySelector('table > tbody')
+        .children[4].children[0]
+        .textContent).toMatch('Notes')
+    expect(
+      container.querySelector('table > tbody')
+        .children[4].children[1]
+        .textContent).toMatch(
+      `${sampleAppointments[0].notes}`)
+  });
+
+  it('renders appointment header with time', () => {
+    render(<Appointment
+      customer={sampleAppointments[0].customer}
+      stylist={sampleAppointments[0].stylist}
+      service={sampleAppointments[0].service}
+      notes={sampleAppointments[0].notes}
+      startAt={sampleAppointments[0].startsAt}
+    />);
+    expect(container.querySelector('h4')).not.toBeNull();
+    expect(
+      container.querySelector('h4')
+        .textContent).toMatch(`Today's appointment at ${sampleAppointments[0].startsAt}`)
+  });
+
 });
 
 
