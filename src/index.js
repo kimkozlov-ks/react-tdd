39ba68a9791686cc03fc7a633a656920e2e768ef
kimkozlov <kimkozlov.ks@gmail.com>
2021-12-17 12:35:57 +0200

    Refactoring and rename

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppointmentsDayView } from './Appointment';
 import { sampleAppointments } from './sampleData';
+import {AppointmentsDayView} from "./AppointmentsDayView";
 
 ReactDOM.render(
   <AppointmentsDayView appointments={sampleAppointments} />,
